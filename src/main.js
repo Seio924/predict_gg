1eab6cdf1bbf1bddd182a0b801cba76b20044e75
Seio924 <134473627+Seio924@users.noreply.github.com>
2024-01-21 16:50:33 +0900

    fix: Run in local

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ const path = require("path");
 // modify your existing createWindow() function
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    width: 800,
+    height: 600,
     webPreferences: {
-      preload: path.join(__dirname, "src", "index.tsx"),
+      webSecurity: false,
+      preload: path.join(__dirname, "preload.js"),
     },
   });
-  win.loadFile(path.join(__dirname, "public", "index.html"));
+  win.loadURL("http://localhost:3000");
 }
 app.whenReady().then(() => {
   createWindow();
