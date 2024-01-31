53f43f1df849d22b623ed2b1da59a45170214c84
Seio924 <seio9241@gmail.com>
2024-01-31 14:14:40 +0900

    feat: Preprocess data ( not finished )

--- backend/train.py
+++ backend/train.py
@@ -2,6 +2,14 @@ from utils import PreprocessData
 
 test = PreprocessData('C:\GitHub\predict_gg/backend/api_match_info.json', 'C:\GitHub\predict_gg/backend/api_timeline_info.json')
 
+f = open("log.txt", 'w')
 #test.get_data('젠지 한별')
 
-test.get_item_data()
+#event_list = test.get_event()
+
+participant_frame_list = test.get_participant_frame()
+
+for i in participant_frame_list:
+    f.write(str(i) + '\n')
+
+f.close()
