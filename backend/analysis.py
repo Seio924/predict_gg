3fd2a2d9f4088b4bc4b53d58fec04f4fe0bb7cc2
Seio924 <seio9241@gmail.com>
2024-02-01 23:27:51 +0900

    feat: Analysis gold std_dev

--- backend/analysis.py
+++ backend/analysis.py
@@ -1,48 +1,35 @@
 from utils import PreprocessData
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
-test = PreprocessData('C:/GitHub/predict_gg/backend/api_match_info.json', 'C:/GitHub/predict_gg/backend/api_timeline_info.json')
+fig, axes = plt.subplots(1, 2, sharex=True, sharey=True)
 
-interval_list = test.get_condition_timeline(10000)
-line = test.get_match_data()[-1]
+for i in range(10):
+    test = PreprocessData('C:/GitHub/predict_gg/api_match_info.json', 'C:/GitHub/predict_gg/api_timeline_info.json')
 
-interval_list = np.array(interval_list, dtype=int)
+    interval_list = test.get_condition_timeline(10000)
+    line = test.get_match_data()[-1]
 
-time = interval_list[:, 0]
-team1_gold = interval_list[:, 1:6]
-team2_gold = interval_list[:, 1+9:6+9]
+    interval_list = np.array(interval_list, dtype=int)
 
-team1_std_dev = np.std(team1_gold, axis=1)
-team2_std_dev = np.std(team2_gold, axis=1)
+    time = interval_list[:, 0]
+    team1_gold = interval_list[:, 1:6]
+    team2_gold = interval_list[:, 1+9:6+9]
 
-team1_mean = []
-team2_mean = []
+    team1_std_dev = np.std(team1_gold, axis=1)
+    team2_std_dev = np.std(team2_gold, axis=1)
 
-print(team1_std_dev)
+    axes[0].plot(time, team1_std_dev, label=f'Team 1 std_dev')
+    axes[1].plot(time, team2_std_dev, label=f'Team 2 std_dev')
 
-fig, axes = plt.subplots(1, 2, sharex=True, sharey=True)  # 두 개의 서브플롯 생성
+    time.sleep(2)
 
-for i in range(5):
-    axes[0].plot(time, team1_gold[:, i], label=f'Team 1 {line[i+1]}')
-
-
-axes[0].plot(time, team1_std_dev, label=f'Team 1 mean')
-
-
-for i in range(5):
-    axes[1].plot(time, team2_gold[:, i], label=f'Team 2 {line[i+6]}')
-
-
-axes[1].plot(time, team2_std_dev, label=f'Team 1 mean')
-
-
-
-axes[1].set_xlabel('Time')  # 공유 x 축을 가진 두 번째 서브플롯만 x 축 레이블을 표시
+axes[1].set_xlabel('Time')
 axes[0].set_ylabel('Gold')
 axes[1].set_ylabel('Gold')
 
-fig.suptitle('Gold Over Time')  # 전체 제목
+fig.suptitle('Team gold std_dev')
 
 axes[0].legend()
 axes[0].grid(True)
