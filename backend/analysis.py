5f8d26dbf4242bcf4505b89c08fa7b9f09d4757f
Seio924 <seio9241@gmail.com>
2024-02-03 16:58:28 +0900

    fix: Analysis.py gold list

--- backend/analysis.py
+++ backend/analysis.py
@@ -3,6 +3,28 @@ import numpy as np
 import time as t
 from utils import PreprocessData
 
+TEAM1_TOP_GOLD = 1
+TEAM1_TOP_POTION = 2
+TEAM1_TOP_KDA = 3
+
+TEAM1_JUNGLE_GOLD = 4
+TEAM1_JUNGLE_POTION = 5
+TEAM1_JUNGLE_KDA = 6
+
+TEAM1_MIDDLE_GOLD = 7
+TEAM1_MIDDLE_POTION = 8
+TEAM1_MIDDLE_KDA = 9
+
+TEAM1_BOTTOM_GOLD = 10
+TEAM1_BOTTOM_POTION = 11
+TEAM1_BOTTOM_KDA = 12
+
+TEAM1_UTILITY_GOLD = 13
+TEAM1_UTILITY_POTION = 14
+TEAM1_UTILITY_KDA = 15
+
+TEAM_INTERVAL = 26
+
 class AnalysisData:
     def __init__(self):
         self.fig, self.axes = plt.subplots(1, 2, sharex=True, sharey=True)
@@ -18,14 +40,19 @@ class AnalysisData:
 
         team, win_lose, line, aram = test.get_match_data()
 
+        team1_gold = []
+        team2_gold = []
+
         if aram == 1:
             t.sleep(5)
         else:
             interval_list = np.array(interval_list, dtype=int)
 
             self.time = interval_list[:, 0]
-            team1_gold = interval_list[:, 1:6]
-            team2_gold = interval_list[:, 1+9:6+9]
+
+            for j in [TEAM1_TOP_GOLD, TEAM1_JUNGLE_GOLD, TEAM1_MIDDLE_GOLD, TEAM1_BOTTOM_GOLD, TEAM1_UTILITY_GOLD]:
+                team1_gold.append(interval_list[j])
+                team2_gold.append(interval_list[j+TEAM_INTERVAL])
 
             team1_std_dev = np.std(team1_gold, axis=1)
             team2_std_dev = np.std(team2_gold, axis=1)
