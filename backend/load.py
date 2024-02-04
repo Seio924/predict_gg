a80e8a14be5cfa85654d31b909068574d02a32af
mulkong-21 <rkdrnqls021018@naver.com>
2024-02-04 18:33:34 +0900

    feat: Preprocess data (not finished)

--- backend/load.py
+++ backend/load.py
@@ -1,8 +1,10 @@
 from load_data import LoadData
 
 
-api_key = 'RGAPI-3cfedfee-6699-4af0-8139-28420199de7a'
+api_key = 'RGAPI-59ffd1a9-2677-40b0-be5d-24151cd90ec9'
 
 load_instance = LoadData(api_key)
 
-load_instance.process_challenger_data(180)
+load_instance.process_challenger_data(65)
+
+#load_instance.get_challenger_info()
--- backend/load_data.py
+++ backend/load_data.py
@@ -33,14 +33,50 @@ class LoadData():
     def get_challenger_info(self):
         challenger_url = "https://kr.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=" + self.api_key
         r6 = requests.get(challenger_url)
-        entries = r6.json()['entries']
-        self.summonerId = [i['summonerName'] for i in entries if 'summonerName' in i]
-
+        self.summonerId = [entry['summonerName'] for entry in r6.json()['entries'] if entry['summonerName'] != '']
+        self.summonerId.sort()
         with open('backend/api_challenger_info.json', 'w', encoding='utf-8') as json_file:
-            json.dump(r6.json(), json_file, ensure_ascii=False, indent=4)
+            json.dump(self.summonerId, json_file, ensure_ascii=False, indent=4)
+
+    def get_diamond1_info(self):
+        diamond1_url = "https://kr.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/DIAMOND/I?page=1&api_key=" + self.api_key
+        r7 = requests.get(diamond1_url)
+        self.summonerId = [entry['summonerName'] for entry in r6.json()['entries'] if entry['summonerName'] != '']
+        self.summonerId.sort()
+
+        with open('backend/api_diamond1_info.json', 'w', encoding='utf-8') as json_file:
+            json.dump(self.summonerId, json_file, ensure_ascii=False, indent=4)
 
     def process_challenger_data(self, num_matches):
         self.get_challenger_info()
+        analysis_data = AnalysisData()
+
+        num = 0
+        for summoner_name in self.summonerId:
+            puuid = self.get_puuid(summoner_name)
+            match_ids = self.get_matchid(puuid)
+    
+            for match_id in match_ids:
+                
+                self.get_match_data(match_id)
+                self.get_timeline_data(match_id)
+                num += 1
+                analysis_data.analyze_data()
+                
+                print("분석 완료")
+                time.sleep(5)
+
+                if num == num_matches:
+                    break
+                
+            
+            if num == num_matches:
+                break
+        analysis_data.plot_results()
+        print("끝")
+
+    def process_diamond1_data(self, num_matches):
+        self.get_diamond1_info()
         analysis_data = AnalysisData()
 
         num = 0
