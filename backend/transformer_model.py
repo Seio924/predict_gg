c5477858b2946194653f8e6b36c8264e11418da5
Seio924 <seio9241@gmail.com>
2024-02-09 01:35:25 +0900

    fix: Model design (exclude decode)

--- backend/transformer_model.py
+++ backend/transformer_model.py
@@ -1,38 +1,107 @@
 import tensorflow as tf
 import numpy as np
 
-def positional_encoding(data):
-    n, dim = data.shape
-    pos_enc = np.zeros((n, dim))
+class PositionalEncoding(tf.keras.layers.Layer):
+    def __init__(self):
+        super(PositionalEncoding, self).__init__()
 
-    for i in range(dim):
-        if i % 2 == 0:
-            pos_enc[:, i] = np.sin(np.arange(0, n) / 10000**(2 * i / dim))
-        else:
-            pos_enc[:, i] = np.cos(np.arange(0, n) / 10000**(2 * (i - 1) / dim))
+    def call(self, data):
+        n, dim = data.shape
+        pos_enc = np.zeros((n, dim))
 
-    for i in range(n):
-        if i % 2 == 0:
-            pos_enc[i, :] *= np.sin(np.arange(0, dim) / 10000**(2 * i / n))
-        else:
-            pos_enc[i, :] *= np.cos(np.arange(0, dim) / 10000**(2 * (i - 1) / n))
+        for i in range(dim):
+            if i % 2 == 0:
+                pos_enc[:, i] = np.sin(np.arange(0, n) / 10000**(2 * i / dim))
+            else:
+                pos_enc[:, i] = np.cos(np.arange(0, n) / 10000**(2 * (i - 1) / dim))
 
-    return pos_enc
+        for i in range(n):
+            if i % 2 == 0:
+                pos_enc[i, :] *= np.sin(np.arange(0, dim) / 10000**(2 * i / n))
+            else:
+                pos_enc[i, :] *= np.cos(np.arange(0, dim) / 10000**(2 * (i - 1) / n))
 
-def add_positional_encoding(data):
-    pos_enc = positional_encoding(data)
-    return data + pos_enc
+        return data + pos_enc
 
-def linear_layer(data, units):
-    data_with_pos_enc = add_positional_encoding(data)
-    linear_model = tf.keras.Sequential([
-        tf.keras.layers.Dense(units)
-    ])
-    return linear_model(data_with_pos_enc)
+class ScaledDotProductAttention(tf.keras.layers.Layer):
+    def __init__(self):
+        super(ScaledDotProductAttention, self).__init__()
 
-# 예제 데이터
-data = np.array([[1000, 500, 100, 500], [2000, 550, 150, 500]])
+    def call(self, query, key, value):
+        # query와 key를 내적하여 attention score 계산
+        attention_score = tf.matmul(query, key, transpose_b=True)
+        
+        # scale factor로 나누어줌
+        d_model = tf.cast(tf.shape(key)[-1], tf.float32)
+        attention_score /= tf.math.sqrt(d_model)
+        
+        # softmax 함수를 사용하여 attention score 계산 -> 0~1사이로 정규화
+        attention_weights = tf.nn.softmax(attention_score, axis=-1)
+        
+        # attention score와 value를 내적하여 self-attention value 계산
+        self_attention_value = tf.matmul(attention_weights, value)
+        
+        return self_attention_value
 
-# Linear 연산을 수행한 결과 출력
-linear_result = linear_layer(data, units=10)
-print(linear_result)
+class TransformerLayer(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads):
+        super(TransformerLayer, self).__init__()
+        self.query_dense = tf.keras.layers.Dense(d_model)
+        self.key_dense = tf.keras.layers.Dense(d_model)
+        self.value_dense = tf.keras.layers.Dense(d_model)
+        self.attention = ScaledDotProductAttention()
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+    def call(self, data, look_ahead_mask=None):
+        data_with_pos_enc = PositionalEncoding()(data)
+
+        # query, key, value의 차원을 d_model로 줄이기 -> Linear layer와 곱하기
+        query = self.query_dense(data_with_pos_enc)
+        key = self.key_dense(data_with_pos_enc)
+        value = self.value_dense(data_with_pos_enc)
+        
+         # 다음 연산을 위해 각각의 차원을 num_heads로 나누기
+        query = tf.concat(tf.split(query, self.num_heads, axis=-1), axis=0)
+        key = tf.concat(tf.split(key, self.num_heads, axis=-1), axis=0)
+        value = tf.concat(tf.split(value, self.num_heads, axis=-1), axis=0)
+        
+        # scaled_dot_product_attention 함수를 호출하여 self-attention value 계산
+        self_attention_value = self.attention(query, key, value)
+
+        # 룩어헤드 마스크 적용 (현재 시점에서 이후의 시점에 대한 정보를 모델이 보지못하도록)
+        if look_ahead_mask is not None:
+            self_attention_value *= look_ahead_mask
+        
+        return self_attention_value
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        self.query_dense = tf.keras.layers.Dense(d_model)
+        self.key_dense = tf.keras.layers.Dense(d_model)
+        self.value_dense = tf.keras.layers.Dense(d_model)
+        self.attention = ScaledDotProductAttention()
+        self.final_dense = tf.keras.layers.Dense(d_model)
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+    def call(self, data):
+        # Multi-head self-attention 수행을 위해 query, key, value 생성
+        query = self.query_dense(data)
+        key = self.key_dense(data)
+        value = self.value_dense(data)
+
+        # 다음 연산을 위해 각각의 차원을 num_heads로 나누기
+        query = tf.concat(tf.split(query, self.num_heads, axis=-1), axis=0)
+        key = tf.concat(tf.split(key, self.num_heads, axis=-1), axis=0)
+        value = tf.concat(tf.split(value, self.num_heads, axis=-1), axis=0)
+        
+        # scaled_dot_product_attention 함수를 호출하여 각 head 별 self-attention value 계산
+        self_attention_value = self.attention(query, key, value)
+        
+        # 각 head의 self-attention value를 다시 합치기
+        concat_attention = tf.concat(tf.split(self_attention_value, self.num_heads, axis=0), axis=-1)
+        
+        # 마지막 linear layer 적용
+        return self.final_dense(concat_attention)
