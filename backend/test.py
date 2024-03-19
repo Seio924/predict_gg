f16b956c6c7846fddded5ae5ec2433891654b796
mulkong-21 <rkdrnqls021018@naver.com>
2024-03-19 23:43:37 +0900

    fix: data 2016 split

--- backend/test.py
+++ backend/test.py
@@ -18,8 +18,6 @@ def read_win_lose_file(filename):
     return result
 
             
-
-
 if __name__ == "__main__":
     interval_files = ['api_interval_list1-1', 'api_interval_list1-2', 'api_interval_list1-3', 'api_interval_list1-4', 'api_interval_list1-5', 'api_interval_list1-6', 'api_interval_list1-7', 'api_interval_list1-8',
                      'api_interval_list2-1', 'api_interval_list2-2', 'api_interval_list2-3', 'api_interval_list2-4', 'api_interval_list2-5', 'api_interval_list2-6', 'api_interval_list2-7', 'api_interval_list2-8',
@@ -29,6 +27,7 @@ if __name__ == "__main__":
                      'api_win_lose_list2-1', 'api_win_lose_list2-2', 'api_win_lose_list2-3', 'api_win_lose_list2-4', 'api_win_lose_list2-5', 'api_win_lose_list2-6', 'api_win_lose_list2-7', 'api_win_lose_list2-8',
                      'api_win_lose_list3-1', 'api_win_lose_list3-2', 'api_win_lose_list3-3', 'api_win_lose_list3-4', 'api_win_lose_list3-5', 'api_win_lose_list3-6', 'api_win_lose_list3-7', 'api_win_lose_list3-8']
     
+    a = 1; b = 1
     
     for interval_file, win_lose_file in zip(interval_files, win_lose_files):
         train_data = []
@@ -40,26 +39,25 @@ if __name__ == "__main__":
         train_data += read_interval_file("api_data/data/" + interval_file + ".txt")
         win_lose_list += read_win_lose_file("api_data/data/" + win_lose_file + ".txt")
 
-        for train_data1, win_lose_list1 in zip(train_data, win_lose_list):
-            a = 1
+        for train_data1 in train_data: # 한게임
             for d in range(len(train_data1)):
-                b = 1
-                if len(train_data_split) == 63:
-                    with open("api_data/data_tmp/interval_split_" + str(a) + "_" + str(b) + ".txt", 'w') as file:
-                        # 파일에 내용 쓰기
-                        file.write(train_data_split)
+                train_data_split.append(train_data1[:d+1])
 
-                    with open("api_data/data_tmp/win_lose_split_" + str(a) + "_" + str(b) + ".txt", 'w') as file:
+                if len(train_data_split) == 63:
+                    with open("api_data/data_tmp/interval_tmp_" + str(a) + "_" + str(b) + ".txt", 'w') as file:
                         # 파일에 내용 쓰기
-                        file.write(win_lose_list_split)
+                        file.write(str(train_data_split))
 
                     train_data_split = []
-                    win_lose_list_split = []
                     b += 1
-                
-                train_data_split.append(train_data1[:d+1])
-                win_lose_list_split.append(win_lose_list1[:d+1])
-            a += 1
 
+        if len(train_data_split) != 0: #이건 맞는지 모르겠다
+            with open("api_data/data_tmp/interval_tmp_" + str(a) + "_" + str(b) + ".txt", 'w') as file:
+                # 파일에 내용 쓰기
+                file.write(str(train_data_split))
+
+        print("파일 한개 완료")
+        a += 1
+        b = 1
         
 
